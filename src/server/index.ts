9c317d357616af6ad8b8617d2df28cfc680d91b0
Hugo <381093478@qq.com>
2024-08-04 23:20:45 +0800

    Test v0.4

--- src/server/index.ts
+++ src/server/index.ts
@@ -5,4 +5,9 @@ const app = express()
 app.use(api)
 app.use(express.static(process.cwd() + '/dist'))
 
+app.get('*', (req, res) => {
+  console.log('Request URL:', req.url)
+  res.sendFile(process.cwd() + '/dist/index.html')
+})
+
 app.listen(3002, () => console.log('Server started'))
