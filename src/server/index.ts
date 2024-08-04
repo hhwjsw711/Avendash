bd21503fdf65d9cacbcda84b94c32fd5df1819e0
Hugo <381093478@qq.com>
2024-08-04 23:21:29 +0800

    Test v0.5

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import { api } from './api'
 
 const app = express()
-app.use(api)
+app.use('/api', api)
 app.use(express.static(process.cwd() + '/dist'))
 
 app.get('*', (req, res) => {
